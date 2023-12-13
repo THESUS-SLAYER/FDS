bc1ab4d7aa04346848b9b1fe1b73ad1980184206
THESUSSLAYER <153382181+THESUSSLAYER@users.noreply.github.com>
2023-12-13 18:09:10 +0530

    Update queuejob.cpp

--- queuejob.cpp
+++ queuejob.cpp
@@ -1,97 +1,66 @@
 #include <iostream>
-#define max 10
+#define MAX 10
 using namespace std;
-
-struct queue{
-	int data[max];
-	int front,rear;
+struct queue
+{ int data[MAX];
+int front,rear;
 };
-
-class Queue{
-	struct queue q;
-	public :
-		Queue(){
-			q.front=q.rear=-1;
-		}
-		int isempty();
-		int isfull();
-		void enqueue(int);
-		int delqueue();
-		void display();
+class Queue
+{ struct queue q;
+public:
+Queue(){q.front=q.rear=-1;}
+int isempty();
+int isfull();
+void enqueue(int);
+int delqueue();
+void display();
 };
-
-int Queue::isempty(){
-	return (q.front==q.rear)?1:0;
+int Queue::isempty()
+{
+return(q.front==q.rear)?1:0;
 }
-int Queue::isfull(){
-	return (q.rear==max-1)?1:0;
+int Queue::isfull()
+{ return(q.rear==MAX-1)?1:0;}
+void Queue::enqueue(int x)
+{q.data[++q.rear]=x;}
+int Queue::delqueue()
+{return q.data[++q.front];}
+void Queue::display()
+{ int i;
+cout<<"\n";
+for(i=q.front+1;i<=q.rear;i++)
+cout<<q.data[i]<<" ";
 }
-void Queue::enqueue(int x){
-	q.data[++q.rear]=x;
+int main()
+{ Queue obj;
+int ch,x;
+do{ cout<<"\n 1. insert job\n 2.delete job\n 3.display\n 4.Exit\n Enter your choice:";
+cin>>ch;
+switch(ch)
+{ case 1: if (!obj.isfull())
+{ cout<<"\n Enter data:";
+cin>>x;
+obj.enqueue(x);
 }
-int Queue::delqueue(){
-	return (q.data[++q.front]);
+else
+cout<< "Queue is overflow";
+break;
+case 2: if(!obj.isempty())
+cout<<"\n Deleted Element="<<obj.delqueue();
+else
+{ cout<<"\n Queue is underflow"; }
+cout<<"\nremaining jobs :";
+obj.display();
+break;
+case 3: if (!obj.isempty())
+{ cout<<"\n Queue contains:";
+obj.display();
 }
-void Queue::display(){
-	int i;
-	cout<<"\n";
-	for(i=q.front+1;i<=q.rear;i++){
-		cout<<q.data[i]<<" ";
-	}
+else
+cout<<"\n Queue is empty";
+break;
+case 4: cout<<"\n Exit";
 }
-
-int main(){
-	Queue obj;
-	int ch,x;
-	
-	do{
-		cout<<"\n MENU";
-		cout<<"\n 1.insert";
-		cout<<"\n 2.delete";
-		cout<<"\n 3.display";
-		cout<<"\n 4.Exit";
-		cout<<"\n Enter your choice : ";
-		cin>>ch;
-		
-		switch(ch){
-			case 1:
-				if(!obj.isfull() ){
-					cout<<"\n Enter job data : ";
-					cin>>x;
-					obj.enqueue(x);
-				}
-				else
-				{
-					cout<<"\n job is overflow ";
-				}
-				break;
-				
-			case 2:
-				if(!obj.isempty() ){
-					cout<<"\n Delete job data : "<<obj.delqueue();
-				}
-				else
-				{
-					cout<<"\n job is underflow : ";
-				}
-				cout<<"\n Remaing jobs : ";
-				obj.display();
-				break ;
-				
-			case 3:
-				if(!obj.isempty() ){
-					cout<<"\n Job Contains : ";
-					obj.display();
-				}
-				else 
-				{
-					cout<<"\n Job is underflow ";
-				}
-				break ;
-				
-			case 4:
-				cout<<"\n Exit";
-		}
-	}while(ch!=4);
-
+}while(ch!=4);
+return 0;
 }
